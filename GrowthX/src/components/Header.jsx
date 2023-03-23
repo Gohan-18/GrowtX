01aa753464edc65719a42a90d56f4c0cf71a5ba7
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-23 20:52:29 +0530

    Landing page done

--- GrowthX/src/components/Header.jsx
+++ GrowthX/src/components/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Box, LinearProgress } from '@mui/material'
+import React, { useState } from 'react';
+import GrowthXLogo from '../assets/Dark background full logo.svg'
 
 const Header = () => {
+
+    const [progress, setProgress] = useState(50)
+
   return (
-    <div>Header</div>
+    <Box sx={{position: 'fixed', top: 0, left: 0, right: 0}} >
+    <LinearProgress variant="determinate" value={progress} />
+    <Box sx={{width: '100%', height: '60px', display: 'flex', alignItems: 'center', justifyContent: 'start', px: '16px', backgroundColor: '#121212'}} >
+        <img  style={{width: '100px'}} src={GrowthXLogo} alt='GrowthX logo' />
+    </Box>
+    </Box>
   )
 }
 
--- GrowthX/src/pages/Home.jsx
+++ GrowthX/src/pages/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { Typography, Box, Container, Button } from "@mui/material";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+          alignItems: "start",
+          justifyContent: "center",
+          flexDirection: "column",
+          px: {
+            xs: "30px",
+            md: "50px",
+          },
+        }}
+      >
+        <Typography
+          gutterBottom
+          sx={{ color: "#fff", fontSize: { xs: "20px", md: "24px" } }}
+        >
+          Up-skilling requires time commitment
+        </Typography>
+        <Typography
+        gutterBottom
+          sx={{
+            color: "rgba(255, 255, 255, 0.7)",
+            fontSize: { xs: "16px", md: "20px" },
+          }}
+        >
+          The GrowthX experience is designed by keeping in mind the working
+          hours founders & full time operators typically work in.
+        </Typography>
+        <Typography
+          sx={{
+            color: "rgba(255, 255, 255, 0.7)",
+            fontSize: { xs: "16px", md: "20px" },
+            py: '20px'
+          }}
+        >
+          You will spend
+          <span style={{display: 'block'}} >
+          - 6 hours/week for the first 5 weeks
+          </span>
+          <span style={{display: 'block'}}>
+          - 15 hours/week for the last 3 weeks
+          </span>
+        </Typography>
+        <Button variant="contained" sx={{backgroundColor: 'rgb(0, 119, 255)', color: '#fff', fontWeight: '500'}} >I agree</Button>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
